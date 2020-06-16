5685d98487308bc06aa6147753197e5d83900920
Taciano Dreckmann Perez <tdperez@hotmail.com>
2020-06-16 21:57:01 +0200

    introduced <Block> element to Story DOM

--- story-dom/src/test/java/com/o3/storyinspector/storydom/io/XmlWriterTest.java
+++ story-dom/src/test/java/com/o3/storyinspector/storydom/io/XmlWriterTest.java
@@ -1,5 +1,6 @@
 package com.o3.storyinspector.storydom.io;
 
+import com.o3.storyinspector.storydom.Block;
 import com.o3.storyinspector.storydom.Book;
 import com.o3.storyinspector.storydom.Chapter;
 import junitx.framework.FileAssert;
@@ -15,14 +16,20 @@ class XmlWriterTest {
     @org.junit.jupiter.api.Test
     void testExportBookToXmlFile() throws JAXBException {
         // given
-        Book book = new Book();
-        Chapter chapter1 = new Chapter();
+        final Book book = new Book();
+        final Chapter chapter1 = new Chapter();
         chapter1.setTitle("Chapter 1 - The first test");
-        chapter1.setBody("This is the body of chapter 1.");
+        final Block block1 = new Block();
+        block1.setWordCount("100");
+        block1.setBody("This is the body of chapter 1.");
+        chapter1.getBlocks().add(block1);
         book.getChapters().add(chapter1);
-        Chapter chapter2 = new Chapter();
+        final Chapter chapter2 = new Chapter();
         chapter2.setTitle("Chapter 2 - The test continues");
-        chapter2.setBody("This is the body of chapter 2.");
+        final Block block2 = new Block();
+        block2.setSentimentScore("-1");
+        block2.setBody("This is the body of chapter 2.");
+        chapter2.getBlocks().add(block2);
         book.getChapters().add(chapter2);
 
         // when

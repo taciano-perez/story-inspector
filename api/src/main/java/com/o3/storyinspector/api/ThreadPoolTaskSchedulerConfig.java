2865facfc9812a315b34ab61e5cac12cb5d0b112
Taciano Dreckmann Perez <tdperez@hotmail.com>
2021-01-02 17:51:18 +0100

    introduced scheduling of book processing (thread pool of one thread)

---
+++ api/src/main/java/com/o3/storyinspector/api/ThreadPoolTaskSchedulerConfig.java
@@ -0,0 +1,21 @@
+package com.o3.storyinspector.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+@ComponentScan(basePackages = "com.o3.storyinspector.api")
+public class ThreadPoolTaskSchedulerConfig {
+
+    private static final int THREAD_POOL_SIZE = 1;
+
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(THREAD_POOL_SIZE);
+        threadPoolTaskScheduler.setThreadNamePrefix("ProcessBookTaskScheduler");
+        return threadPoolTaskScheduler;
+    }
+}
